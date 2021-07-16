393597ac33223d728d29a8a68dbf12de8ae7744c
simon jonah <39828209+Simonjonah@users.noreply.github.com>
2021-07-15 17:49:28 -0800

    Upload zip file and unpacked
    Uploading of zip file and unpacked

--- admin/layouts/templates-settings-and-importer.php
+++ admin/layouts/templates-settings-and-importer.php
@@ -1,6 +1,5 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	
 	if($_FILES["templates"]["name"]) {
 		$filename = $_FILES["templates"]["name"];
 		$source = $_FILES["templates"]["tmp_name"];
@@ -14,30 +13,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				break;
 			} 
 		}
+		
 		$continue = strtolower($name[1]) == 'zip' ? true : false;
 		if(!$continue) {
 			echo "The file you are trying to upload is not a .zip file. Please try again.";
 		}
-		$target_path = '../wp-content/plugins/rimplenet-templates/public'.$filename;  // change this to the correct site
+		$target_path_dir =  plugin_dir_path(dirname(__DIR__ ))."public/templates";
+		$target_path =  $target_path_dir."/$filename";
 		if(move_uploaded_file($source, $target_path)) {
 			$zip = new ZipArchive();
 			$x = $zip->open($target_path);
 			if ($x === true) {
-				$zip->extractTo('../wp-content/plugins/rimplenet-templates/public'); // change this to the correct
+				$zip->extractTo($target_path_dir); // extract zip to the directory
 				$zip->close();
 		
 				unlink($target_path);
 			}
-			echo "Your .zip file was uploaded and unpacked.";
+			echo "Your .zip template was uploaded and extracted successfully.";
 		} else {	
 			echo "There was a problem with the upload. Please try again.";
 		}
 	}
 }
-
  ?>
 
-<form action="" method="post" enctype="multipart/form-data">
+<form action="" method="post" accept="zip,application/octet-stream,application/zip,application/x-zip,application/x-zip-compressed" enctype="multipart/form-data">
 	<h4>Upload Your File Here</h4>
 	<input type="file" name="templates"><br><br>
 	
