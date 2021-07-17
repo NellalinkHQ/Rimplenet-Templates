6fe9d46daa5c296fffdae2048cec8512036f5010
simon jonah <39828209+Simonjonah@users.noreply.github.com>
2021-07-17 01:46:04 -0800

    All done and working

--- admin/class-rimplenet-templates-admin.php
+++ admin/class-rimplenet-templates-admin.php
@@ -22,37 +22,37 @@
  */
 class Rimplenet_Templates_Admin {
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
-	 */
-	private $plugin_name;
+    /**
+     * The ID of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $plugin_name    The ID of this plugin.
+     */
+    private $plugin_name;
 
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
-	private $version;
+    /**
+     * The version of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $version    The current version of this plugin.
+     */
+    private $version;
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
-	public function __construct( $plugin_name, $version ) {
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @since    1.0.0
+     * @param      string    $plugin_name       The name of this plugin.
+     * @param      string    $version    The version of this plugin.
+     */
+    public function __construct( $plugin_name, $version ) {
 
-		$this->plugin_name = $plugin_name;
-		$this->version = $version;
-		$this->load_dependencies();
-		
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
+        $this->load_dependencies();
+        
         add_action( 'admin_menu', array( $this, 'admin_menu_addon_display' ) );
         
         add_action( 'admin_menu', array( $this, 'templates_settings_and_import' ));
@@ -63,14 +63,14 @@ class Rimplenet_Templates_Admin {
         add_action( 'admin_footer', array($this,'make_class_menu_target_blank' ));  
         
         
-		add_action( 'wp_nav_menu_item_custom_fields', array($this,'nav_menu_custom_fields'), 10, 2 );
-		add_filter( 'wp_setup_nav_menu_item',array($this,'nav_menu_setup_custom_fields' ));
-		add_action( 'wp_update_nav_menu_item', array($this,'nav_menu_update'), 10, 2 );
-		
+        add_action( 'wp_nav_menu_item_custom_fields', array($this,'nav_menu_custom_fields'), 10, 2 );
+        add_filter( 'wp_setup_nav_menu_item',array($this,'nav_menu_setup_custom_fields' ));
+        add_action( 'wp_update_nav_menu_item', array($this,'nav_menu_update'), 10, 2 );
+        
 
-	}
-	
-		
+    }
+    
+        
     public function admin_menu_addon_display(){
            
             add_menu_page(
@@ -91,20 +91,20 @@ class Rimplenet_Templates_Admin {
        }
     
       public function templates_settings_and_import()
-	{
-		 add_submenu_page(
+    {
+         add_submenu_page(
             'rimplenet-templates',
             __( 'Templates - Settings & Importer', 'text-domain' ),
             __( 'Templates - Settings & Importer', 'text-domain' ),
             'manage_options',
             admin_url( 'admin.php?page=rimplenet-templates&tab=templates-settings-and-importer')
             );
-		   
-	}
-	
+           
+    }
+    
     public function admin_menu_docs_setup_link ()
-	{
-		 add_submenu_page(
+    {
+         add_submenu_page(
             'rimplenet-templates',
             __( '<strong style="color:#FCB214;" class="submenu-page"> Docs / How to Use</strong>', 'text-domain' ),
             __( '<strong style="color:#FCB214;" class="submenu-page"> Docs /  How to Use</strong>', 'text-domain' ),
@@ -113,8 +113,8 @@ class Rimplenet_Templates_Admin {
             'https://rimplenet.com/docs/templates'
             
             );
-		   
-	}
+           
+    }
 
        
   
@@ -123,8 +123,8 @@ class Rimplenet_Templates_Admin {
     
   
     public function admin_menu_donate_link()
-	{
-		 add_submenu_page(
+    {
+         add_submenu_page(
             'rimplenet-templates',
             __( '<strong style="color:#31a231;" class="submenu-page"> Donate </strong>', 'text-domain' ),
             __( '<strong style="color:#31a231;" class="submenu-page"> Donate </strong>', 'text-domain' ),
@@ -132,9 +132,9 @@ class Rimplenet_Templates_Admin {
             'https://rimplenet.com/donate'
             
             );
-	}
-	
-	function make_class_menu_target_blank()
+    }
+    
+    function make_class_menu_target_blank()
     {
         ?>
         <script type="text/javascript">
@@ -147,45 +147,45 @@ class Rimplenet_Templates_Admin {
     
     
     
-    //On Plugin Activation/Deactivation Page	
-	function my_plugin_settings_link($links) { 
+    //On Plugin Activation/Deactivation Page    
+    function my_plugin_settings_link($links) { 
       
       // Build and escape the URL.
-    	// Create the link.
-    	$settings_link = "<a href='".admin_url( 'admin.php?page=rimplenet-templates')."'>" . __( 'Settings' ) . '</a>';
-    	$docs_link = "<a href='https://rimplenet.com/docs/templates'  style='color: #93003c;font-weight: 800;' target='_blank'>" . __( 'Docs / How to Use' ) . '</a>';
-    	$donate_link = "<a href='https://rimplenet.com/donate' style='color: #FCB214;font-weight: 800;' target='_blank'>" . __( 'Donate' ) . '</a>';
-    	// Adds the link to the end of the array.
-    	$added_links = array($settings_link,$docs_link,$donate_link);
-    	
-    	$all_links = array_merge($added_links,$links);
-    	return $all_links;
+        // Create the link.
+        $settings_link = "<a href='".admin_url( 'admin.php?page=rimplenet-templates')."'>" . __( 'Settings' ) . '</a>';
+        $docs_link = "<a href='https://rimplenet.com/docs/templates'  style='color: #93003c;font-weight: 800;' target='_blank'>" . __( 'Docs / How to Use' ) . '</a>';
+        $donate_link = "<a href='https://rimplenet.com/donate' style='color: #FCB214;font-weight: 800;' target='_blank'>" . __( 'Donate' ) . '</a>';
+        // Adds the link to the end of the array.
+        $added_links = array($settings_link,$docs_link,$donate_link);
+        
+        $all_links = array_merge($added_links,$links);
+        return $all_links;
     }
     
     
                
     function nav_menu_custom_fields( $item_id, $item ) {
      
-    	wp_nonce_field( 'rimplenet_menu_meta_nonce', '_rimplenet_menu_meta_nonce_name' );
-    	$custom_menu_meta = get_post_meta( $item_id, '_rimplenet_menu_meta', true );
-    	?>
+        wp_nonce_field( 'rimplenet_menu_meta_nonce', '_rimplenet_menu_meta_nonce_name' );
+        $custom_menu_meta = get_post_meta( $item_id, '_rimplenet_menu_meta', true );
+        ?>
      
-    	<input type="hidden" name="rimplenet-menu-meta-nonce" value="<?php echo wp_create_nonce( 'rimplenet-menu-meta-name' ); ?>" />
+        <input type="hidden" name="rimplenet-menu-meta-nonce" value="<?php echo wp_create_nonce( 'rimplenet-menu-meta-name' ); ?>" />
      
-    	<div class="field-rimplenet_menu_meta description-wide" style="margin: 5px 0;">
-    	    <span class="description"><?php _e('Input BNVB Icon Name - Icons @ <a href="https://material.io/resources/icons/" target="_blank">https://material.io/resources/icons/</a> ', 'rimplenet-menu-meta' ); ?></span>
-    	    <br>
-    	    <input type="hidden" class="nav-menu-id" value="<?php echo $item_id ;?>" />
+        <div class="field-rimplenet_menu_meta description-wide" style="margin: 5px 0;">
+            <span class="description"><?php _e('Input BNVB Icon Name - Icons @ <a href="https://material.io/resources/icons/" target="_blank">https://material.io/resources/icons/</a> ', 'rimplenet-menu-meta' ); ?></span>
+            <br>
+            <input type="hidden" class="nav-menu-id" value="<?php echo $item_id ;?>" />
      
-    	    <div class="logged-input-holder">
-    	        <input class="widefat edit-menu-item-rimplenet" type="text" name="rimplenet_menu_meta[<?php echo $item_id ;?>]" id="rimplenet-menu-meta-for-<?php echo $item_id ;?>" placeholder="account_box" value="<?php echo esc_attr( $custom_menu_meta ); ?>" />
-    	        <label for="rimplenet-menu-meta-for-<?php echo $item_id ;?>">
-    	            <?php _e( 'Input the icon_name, the icon may only appear when used with BNVB dashboard Menu', 'rimplenet-menu-meta'); ?>
-    	        </label>
-    	    </div>
-    	</div>
+            <div class="logged-input-holder">
+                <input class="widefat edit-menu-item-rimplenet" type="text" name="rimplenet_menu_meta[<?php echo $item_id ;?>]" id="rimplenet-menu-meta-for-<?php echo $item_id ;?>" placeholder="account_box" value="<?php echo esc_attr( $custom_menu_meta ); ?>" />
+                <label for="rimplenet-menu-meta-for-<?php echo $item_id ;?>">
+                    <?php _e( 'Input the icon_name, the icon may only appear when used with BNVB dashboard Menu', 'rimplenet-menu-meta'); ?>
+                </label>
+            </div>
+        </div>
      
-    	<?php
+        <?php
     }
     
     function nav_menu_setup_custom_fields($menu_item) {
@@ -195,71 +195,71 @@ class Rimplenet_Templates_Admin {
     
     function nav_menu_update( $menu_id, $menu_item_db_id ) {
      
-    	// Verify this came from our screen and with proper authorization.
-    	if ( ! isset( $_POST['_rimplenet_menu_meta_nonce_name'] ) || ! wp_verify_nonce( $_POST['_rimplenet_menu_meta_nonce_name'], 'rimplenet_menu_meta_nonce' ) ) {
-    		return $menu_id;
-    	}
+        // Verify this came from our screen and with proper authorization.
+        if ( ! isset( $_POST['_rimplenet_menu_meta_nonce_name'] ) || ! wp_verify_nonce( $_POST['_rimplenet_menu_meta_nonce_name'], 'rimplenet_menu_meta_nonce' ) ) {
+            return $menu_id;
+        }
      
-    	if ( isset( $_POST['rimplenet_menu_meta'][$menu_item_db_id]  ) ) {
-    		$sanitized_data = sanitize_text_field( $_POST['rimplenet_menu_meta'][$menu_item_db_id] );
-    		update_post_meta( $menu_item_db_id, '_rimplenet_menu_meta', $sanitized_data );
-    	} else {
-    		delete_post_meta( $menu_item_db_id, '_rimplenet_menu_meta' );
-    	}
+        if ( isset( $_POST['rimplenet_menu_meta'][$menu_item_db_id]  ) ) {
+            $sanitized_data = sanitize_text_field( $_POST['rimplenet_menu_meta'][$menu_item_db_id] );
+            update_post_meta( $menu_item_db_id, '_rimplenet_menu_meta', $sanitized_data );
+        } else {
+            delete_post_meta( $menu_item_db_id, '_rimplenet_menu_meta' );
+        }
     }
     
     private function load_dependencies(){
         
-		//Include file to displays Template Settings Metabox 
-		include_once plugin_dir_path( dirname( __FILE__ ) ) . '/admin/class-template-settings.php';
+        //Include file to displays Template Settings Metabox 
+        include_once plugin_dir_path( dirname( __FILE__ ) ) . '/admin/class-template-settings.php';
     }
          
 
 
-	/**
-	 * Register the stylesheets for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
+    /**
+     * Register the stylesheets for the admin area.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Rimplenet_Templates_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Rimplenet_Templates_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+        /**
+         * This function is provided for demonstration purposes only.
+         *
+         * An instance of this class should be passed to the run() function
+         * defined in Rimplenet_Templates_Loader as all of the hooks are defined
+         * in that particular class.
+         *
+         * The Rimplenet_Templates_Loader will then create the relationship
+         * between the defined hooks and the functions defined in this
+         * class.
+         */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/rimplenet-templates-admin.css', array(), $this->version, 'all' );
+        wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/rimplenet-templates-admin.css', array(), $this->version, 'all' );
 
-	}
+    }
 
-	/**
-	 * Register the JavaScript for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
+    /**
+     * Register the JavaScript for the admin area.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_scripts() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Rimplenet_Templates_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Rimplenet_Templates_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+        /**
+         * This function is provided for demonstration purposes only.
+         *
+         * An instance of this class should be passed to the run() function
+         * defined in Rimplenet_Templates_Loader as all of the hooks are defined
+         * in that particular class.
+         *
+         * The Rimplenet_Templates_Loader will then create the relationship
+         * between the defined hooks and the functions defined in this
+         * class.
+         */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/rimplenet-templates-admin.js', array( 'jquery' ), $this->version, false );
+        wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/rimplenet-templates-admin.js', array( 'jquery' ), $this->version, false );
 
-	}
+    }
 
 }
--- admin/class-template-settings.php
+++ admin/class-template-settings.php
@@ -6,13 +6,18 @@ class Rimplenet_Template_Settings{
     public function __construct() {
         
         $this->viewed_url = $_SERVER['REQUEST_URI'];
-        $this->admin_post_page_type = sanitize_text_field($_GET["rimplenettransaction_type"]);
-        $this->post_id = sanitize_text_field($_GET['post']);
+        //$this->admin_post_page_type = sanitize_text_field($_GET["rimplenettransaction_type"]);
+        $this->post_id = sanitize_text_field($_POST['post_ID']);
         
         add_action('init',  array($this,'load_required_admin_functions'));
+        add_action( 'post_edit_form_tag', array($this, 'rimplenet_post_edit_form_tag'));
         //save meta value with save post hook when Template Settings is POSTED
-        //add_action('save_post_rimplenettransaction',  array($this,'save_settings'), 10,3 );
+        add_action('save_post',  array($this,'save_settings'), 10,3 );
         
+    }
+
+    function rimplenet_post_edit_form_tag() {
+      echo 'enctype="multipart/form-data"';
     }
     
     function load_required_admin_functions() {
@@ -24,7 +29,7 @@ class Rimplenet_Template_Settings{
     
     function rimplenet_template_register_meta_box() {
         
-        add_meta_box( 'rimplenet-admin-wallet-settings-meta-box', esc_html__( 'Wallet Settings', 'rimplenet' ),   array($this,'rimplenet_admin_wallet_meta_box_callback'), 'rimplenettemplate', 'normal', 'high' );
+        add_meta_box( 'rimplenet-admin-wallet-settings-meta-box', esc_html__( 'Template Settings', 'rimplenet' ),   array($this,'rimplenet_admin_wallet_meta_box_callback'), 'rimplenettemplate', 'normal', 'high' );
         add_meta_box( 'rimplenet-admin-wallet-balance-shortcode-meta-box', esc_html__( 'Wallet Balance Shortcode', 'rimplenet' ),   array($this,'rimplenet_admin_wallet_balance_shortcode_meta_box_callback'), 'rimplenettemplate', 'side', 'high' );  
         
     }
@@ -49,55 +54,75 @@ class Rimplenet_Template_Settings{
     
      }
      
-    function save_settings($post_id, $post, $update){
+    function save_settings(int $post_id){
+      $rimplenet_uploaded_dashboard_pic = '';
+      if (isset( $_FILES ) && !empty( $_FILES ) && isset($_FILES['rimplenet_dashboard_pic'])) {
+        $movedfile = wp_handle_upload($_FILES['rimplenet_dashboard_pic'], ['test_form'=>false]);
+        if( $movedfile && !isset($movedfile['error'])) {
+          $rimplenet_uploaded_dashboard_pic = $movedfile['url'];
+        }
+        // Later: if there is an error, return back to user.
+      }
         
-      $rimplenettransaction_type = sanitize_text_field($_POST['rimplenettransaction_type']);
-      if($rimplenettransaction_type=="rimplenet-wallets"){ 
-        $WALLET_CAT_NAME = 'RIMPLENET WALLETS';
-        wp_set_object_terms($post_id, $WALLET_CAT_NAME, 'rimplenettransaction_type');
+      // $rimplenettransaction_type = sanitize_text_field($_POST['rimplenettransaction_type']);
+      // if($rimplenettransaction_type=="rimplenet-wallets"){ 
+      //   $WALLET_CAT_NAME = 'RIMPLENET WALLETS';
+      //   wp_set_object_terms($post_id, $WALLET_CAT_NAME, 'rimplenettransaction_type');
         
-        $rimplenet_wallet_name = get_the_title();
-        $rimplenet_wallet_decimal = sanitize_text_field( $_POST['rimplenet_wallet_decimal'] );
-        $rimplenet_min_withdrawal_amount = sanitize_text_field( $_POST['rimplenet_min_withdrawal_amount'] );
-        $rimplenet_max_withdrawal_amount = sanitize_text_field( $_POST['rimplenet_max_withdrawal_amount'] );
-        $rimplenet_wallet_symbol = sanitize_text_field( $_POST['rimplenet_wallet_symbol'] );
-        $rimplenet_wallet_symbol_position = sanitize_text_field( $_POST['rimplenet_wallet_symbol_position'] );
-        $include_in_withdrawal_form = sanitize_text_field( $_POST['include_in_withdrawal_form'] );
-        $include_in_woocommerce_currency_list = sanitize_text_field( $_POST['include_in_woocommerce_currency_list'] );
-        $enable_as_woocommerce_product_payment_wallet = sanitize_text_field( $_POST['enable_as_woocommerce_product_payment_wallet'] );
-        $rimplenet_wallet_id = sanitize_text_field( $_POST['rimplenet_wallet_id'] );
-        $rimplenet_wallet_type = sanitize_text_field( $_POST['rimplenet_wallet_type'] );
-        $rimplenet_wallet_note = sanitize_text_field( $_POST['rimplenet_wallet_note'] );
+        // $rimplenet_wallet_name = get_the_title();
+        // $rimplenet_wallet_decimal = sanitize_text_field( $_POST['rimplenet_wallet_decimal'] );
+        // $rimplenet_min_withdrawal_amount = sanitize_text_field( $_POST['rimplenet_min_withdrawal_amount'] );
+        // $rimplenet_max_withdrawal_amount = sanitize_text_field( $_POST['rimplenet_max_withdrawal_amount'] );
+        // $rimplenet_wallet_symbol = sanitize_text_field( $_POST['rimplenet_wallet_symbol'] );
+        // $rimplenet_wallet_symbol_position = sanitize_text_field( $_POST['rimplenet_wallet_symbol_position'] );
+        // $include_in_withdrawal_form = sanitize_text_field( $_POST['include_in_withdrawal_form'] );
+        // $include_in_woocommerce_currency_list = sanitize_text_field( $_POST['include_in_woocommerce_currency_list'] );
+        $rimplenet_template_footer_text = sanitize_text_field( $_POST['rimplenet_template_footer_text'] );
+        $rimplenet_template_header_text = sanitize_text_field( $_POST['rimplenet_template_header_text'] );
+        $rimplenet_small_title = sanitize_text_field( $_POST['rimplenet_small_title'] );
+        $rimplenet_template_default = sanitize_text_field( $_POST['rimplenet_template_default'] );
         
+        $rimplenet_sidebar_menu = sanitize_text_field( $_POST['rimplenet_sidebar_menu'] );
+        $rimplenet_page_id = sanitize_text_field( $_POST['page_id'] );
         
         $metas = array( 
-              'rimplenet_wallet_name' => $rimplenet_wallet_name,
-              'rimplenet_wallet_decimal' => $rimplenet_wallet_decimal,
-              'rimplenet_min_withdrawal_amount' => $rimplenet_min_withdrawal_amount,
-              'rimplenet_max_withdrawal_amount' => $rimplenet_max_withdrawal_amount,
-              'rimplenet_wallet_symbol' => $rimplenet_wallet_symbol,
-              'rimplenet_wallet_symbol_position' => $rimplenet_wallet_symbol_position,
-              'rimplenet_wallet_id' => strtolower($rimplenet_wallet_id),
-              'include_in_woocommerce_currency_list' => $include_in_woocommerce_currency_list,
-              'enable_as_woocommerce_product_payment_wallet' => $enable_as_woocommerce_product_payment_wallet,
-              'rimplenet_wallet_type' => $rimplenet_wallet_type,
-              'rimplenet_wallet_note' => $rimplenet_wallet_note,
+              'rimplenet_template_footer_text' => $rimplenet_template_footer_text,
+              'rimplenet_template_header_text' => $rimplenet_template_header_text,
+              'rimplenet_small_title' => $rimplenet_small_title,
+              'rimplenet_sidebar_menu' => $rimplenet_sidebar_menu,
+              'rimplenet_template_default' => $rimplenet_template_default,
+
+              'rimplenet_page_id' => $rimplenet_page_id,
+              'rimplenet_dashboard_pic' => $rimplenet_uploaded_dashboard_pic,
+
+              // 'rimplenet_dashboard_pic' => isset($rimplenet_uploaded_dashboard_pic) ? $rimplenet_uploaded_dashboard_pic['name'] : '',
+
+
+              // 'rimplenet_wallet_decimal' => $rimplenet_wallet_decimal,
+              // 'rimplenet_min_withdrawal_amount' => $rimplenet_min_withdrawal_amount,
+              // 'rimplenet_max_withdrawal_amount' => $rimplenet_max_withdrawal_amount,
+              // 'rimplenet_wallet_symbol' => $rimplenet_wallet_symbol,
+              // 'rimplenet_wallet_symbol_position' => $rimplenet_wallet_symbol_position,
+              // 'rimplenet_wallet_id' => strtolower($rimplenet_wallet_id),
+              // 'include_in_woocommerce_currency_list' => $include_in_woocommerce_currency_list,
+              // 'enable_as_woocommerce_product_payment_wallet' => $enable_as_woocommerce_product_payment_wallet,
+              // 'rimplenet_wallet_type' => $rimplenet_wallet_type,
+              // 'rimplenet_wallet_note' => $rimplenet_wallet_note,
               
-              'rimplenettransaction_type' => 'rimplenet-wallets',
+              // 'rimplenettransaction_type' => 'rimplenet-wallets',
             
-              'rimplenet_rules_before_wallet_withdrawal' => $rimplenet_rules_before_wallet_withdrawal,
-              'rimplenet_rules_after_wallet_withdrawal' => $rimplenet_rules_after_wallet_withdrawal,
+              // 'rimplenet_rules_before_wallet_withdrawal' => $rimplenet_rules_before_wallet_withdrawal,
+              // 'rimplenet_rules_after_wallet_withdrawal' => $rimplenet_rules_after_wallet_withdrawal,
             );
             
-         foreach ($metas as $key => $value) {
-          update_post_meta($post_id, $key, $value);
-         }
-        
-       }
-    }
-  
-        
+          foreach ($metas as $key => $value) {
+            update_post_meta($post_id, $key, $value);
+          }
+        }
+      
 }
+
+
 
 
 $Rimplenet_Template_Settings = new Rimplenet_Template_Settings();
